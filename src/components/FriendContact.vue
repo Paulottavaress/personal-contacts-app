19b5df81ea33462659cd61f25fd6bad52b9a790e
Paulo Henrique Tavares de Andrade <ph.and@hotmail.com>
2023-02-22 08:48:56 +0000

    added the app full functionality and basic styling

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,41 @@
+<template>
+    <li>
+      <h2>{{ friend.name }} {{ (friend.isFavorite) ? ('(Favorite)') : '' }}</h2>
+      <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+      <button @click="this.$emit('toggle-favorite', friend.id)">Toggle Favorite</button>
+      <ul :style="{ display: detailsAreVisible ? 'block' : 'none'}">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+      </ul>
+      <button @click="this.$emit('delete-friend', friend.id)">Delete</button>
+    </li>
+</template>
+
+<script>
+  export default {
+    props: {
+      friend: {
+        type: Object,
+        required: true
+      }
+    },
+    emits: [
+      'toggle-favorite',
+      'delete-friend'
+    ],
+    data() {
+      return {
+        detailsAreVisible: false
+      };
+    },
+    methods: {
+      toggleDetails() {
+        this.detailsAreVisible = !this.detailsAreVisible;
+      },
+    }
+  };
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,47 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label>Name</label>
+      <input type="text" name="name" v-model="name">
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="text" name="phone" v-model="phone">
+    </div>
+    <div>
+      <label>Email</label>
+      <input type="email" name="email" v-model="email">
+    </div>
+    <div>
+      <label>Favorito?</label>
+      <input type="checkbox" name="isFavorite" v-model="isFavorite">
+    </div>
+    <button type="submit">Add</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        name: '',
+        phone: '',
+        email: '',
+        isFavorite: false
+      }
+    },
+    emits: ['add-contact'],
+    methods: {
+      submitData () {
+        this.$emit('add-contact', {
+          name: this.name,
+          phone: this.phone,
+          email: this.email,
+          isFavorite: this.isFavorite
+        });
+      }
+    }
+  }
+</script>
+
+<style></style>
